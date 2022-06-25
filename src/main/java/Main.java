d43026ebc770dd85f50b8520d20da62e3ee3e877
evgeniiijjj <evgenijjj78@gmail.com>
2022-06-25 12:58:17 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ThreadGroup group = new ThreadGroup("main group");
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Callable<Integer>> taskList = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
-            new MyThread(group, "thread" + i).start();
+            taskList.add(() -> {
+                String name = Thread.currentThread().getName();
+                int j = 0;
+                for ( ;j < 3; j++) {
+                    System.out.println("Я " + name + " всем привет!");
+                    Thread.sleep(2000);
+                }
+                return j;
+            });
         }
-        Thread.sleep(15000);
-        group.interrupt();
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> list = pool.invokeAll(taskList);
+        int result = 0;
+        while (list.size() > 0) {
+            Iterator<Future<Integer>> it = list.iterator();
+            while (it.hasNext()) {
+                Future<Integer> future = it.next();
+                if (future.isDone()) {
+                    result += future.get();
+                    it.remove();
+                }
+            }
+        }
+        pool.shutdown();
+        System.out.println("Выведено " + result + " сообщений");
     }
 }
